fc3621216bd1a27bbfa805efc157a3bd8251215c
Venkatesh <venkatesh071610@gmail.com>
2026-06-10 23:39:50 +0530

    created welcome file

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export default function Test() {
+  return (
+    <div style={{ padding: '24px', textAlign: 'center' }}>
+      <h1>Welcome to the Gateway App!</h1>
+      <p>This is your new feature branch: us123-create-new-file.</p>
+    </div>
+  );
+}
