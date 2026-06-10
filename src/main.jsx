6592119ffc655ec21e7193866233b5bb1cb27855
venkatesh <147084398+venky44555@users.noreply.github.com>
2026-06-10 23:48:18 +0530

    Remove unused variable 'hello' from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-const hello = 'Hello, World!';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
