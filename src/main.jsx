3e95824987379e9acb2be863e7c0c89061f91906
Venkatesh <venkatesh071610@gmail.com>
2026-06-10 23:44:48 +0530

    file chang

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const hello = 'Hello, World!';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
